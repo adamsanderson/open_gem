0bfa613bddab69f701ce933999a61e277aa3438a
Adam Sanderson <netghost@gmail.com>
2009-04-26 08:31:54 -0700

    allow alternate commands

--- lib/rubygems/commands/open_command.rb
+++ lib/rubygems/commands/open_command.rb
@@ -3,25 +3,29 @@ require 'rubygems/dependency'
 
 class Gem::Commands::OpenCommand < Gem::Command
   def initialize
-    super 'open', "Opens the gem's source directory with $EDITOR"
+    super 'open', "Opens the gem's source directory with $EDITOR", :command => nil
+    add_option('-c', '--command COMMAND',
+               'Execute command at path of the rubygem') do |value, options|
+      options[:command] = value
+    end
   end
   
   def arguments # :nodoc:
     "GEMNAME       gem to open"
   end
 
-  def execute
+  def execute    
     name = get_one_gem_name
     
     dep = Gem::Dependency.new name, Gem::Requirement.default
     specs = Gem.source_index.search dep
-
+    
     # TODO: ask which to open:
     path = specs.last.full_gem_path
     
-    editor = ENV['EDITOR']
+    editor = options[:command] || ENV['EDITOR']
     if !editor
-      say "no editor set in $EDITOR"
+      say "Either set $EDITOR, or use -c <command_name>"
     else
       system(editor, path)
     end
