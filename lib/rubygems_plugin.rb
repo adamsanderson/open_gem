5d8081c71896efcc4b2335eb8b287d942b10edd6
Andre Arko <website+github.com@arko.net>
2011-07-12 22:59:48 -0700

    Require common_options directly, so that Rubygems won't try adding anything to the LOAD_PATH.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -5,7 +5,7 @@ require 'rubygems/dependency'
 require 'rubygems/version_option'
 
 require 'shellwords'
-require 'open_gem/common_options'
+require File.expand_path('../open_gem/common_options', __FILE__)
 
 Gem::CommandManager.instance.register_command :open
 Gem::CommandManager.instance.register_command :read
